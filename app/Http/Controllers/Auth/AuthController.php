dab1042c6700eaabdfdef658bdd0f840f8fbaba1
madhusoodhananer <54470746+madhusoodhananer@users.noreply.github.com>
2023-03-19 04:55:10 +0530

    login implemented

---
+++ app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\SchoolController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Cookie;
+use Symfony\Component\HttpFoundation\Response;
+
+class AuthController extends SchoolController
+{
+    public function login(Request $request)
+    {
+
+        try {
+            Validator::make($request->all(),[
+                'username' => 'required',
+                'password' => 'required'
+            ])->validate();
+            $success = [];
+
+            if(Auth::attempt([
+                'username' => $request->username,
+                'password' => $request->password
+            ])){
+                $user = Auth::user();
+                $success = [
+                    'token' => $user->createToken('API Token')->plainTextToken,
+                    'name'  => $user->name
+                ];
+                return $this->sendSuccessResponse($success,'User loggined successfully');
+            }
+            else{
+                return response()->json([
+                    'message' => 'Incorrect username or password',
+                    'code'    => Response::HTTP_UNAUTHORIZED
+                ]);
+            }
+        } catch (\Throwable $th) {
+            return $this->handleApiException($th);
+        }
+    }
+
+    public function logout(Request $request)
+    {
+        try {
+            auth()->user()->tokens()->delete();
+            $cookie = Cookie::forget(config('session.cookie'));
+            return $this->sendSuccessResponse(["cookie" => $cookie],'User logged out successfully');
+        } catch (\Throwable $exception) {
+            return $exception;
+            return $this->HandleApiException($exception);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +14,16 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+
+Route::controller(AuthController::class)
+    ->prefix('auth')
+    ->as('auth.')
+    ->group(function(){
+        Route::post('login',[AuthController::class, 'login'])->name('login');
+        Route::middleware('auth:sanctum')->group(function(){
+            Route::post('logout', [AuthController::class,'logout'])->name('logout');
+        });
+    });
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
